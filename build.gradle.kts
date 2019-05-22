5c3c5b017793878d72ba3cbd8b56b54b8d363c24
Jens Brimfors <jens.brimfors@nordnet.se>
2019-05-22 08:37:59 +0200

    Set name, group and version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,28 @@
 plugins {
     kotlin("jvm") version "1.3.31"
-	jacoco
-	id("org.sonarqube") version "2.7.1"
+    jacoco
+    id("org.sonarqube") version "2.7.1"
 }
+
+group = "se.jensim.reflekt"
+version = findProperty("releaseVersion") ?: "DEV"
+
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 dependencies {
-	implementation(kotlin("stdlib-jdk8"))
-	implementation(kotlin("reflect"))
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
 
-	implementation("org.javassist:javassist:3.25.0-GA")
+    implementation("org.javassist:javassist:3.25.0-GA")
 
-	testImplementation(kotlin("test-junit"))
+    testImplementation(kotlin("test-junit"))
 }
 
 tasks.jacocoTestReport {
-	reports {
-		xml.isEnabled = true
-		csv.isEnabled = false
-		html.destination = file("$buildDir/jacocoHtml")
-	}
+    reports {
+        xml.isEnabled = true
+        csv.isEnabled = false
+        html.destination = file("$buildDir/jacocoHtml")
+    }
 }
