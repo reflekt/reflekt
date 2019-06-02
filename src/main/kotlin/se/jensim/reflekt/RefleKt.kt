231d0f0074b1712f6ffb1a96036ce3a5a47fa675
Jens Brimfors <jens.brimfors@nordnet.se>
2019-06-02 07:26:48 +0200

    Move func away from the Store class, that pattern would introduce unnecessary complexity or bugs

--- src/main/kotlin/se/jensim/reflekt/RefleKt.kt
+++ src/main/kotlin/se/jensim/reflekt/RefleKt.kt
@@ -1,11 +1,14 @@
 package se.jensim.reflekt
 
-import se.jensim.reflekt.internal.ReflektStore.annotatedClasses
 import se.jensim.reflekt.internal.ReflektStore.getClasses
-import se.jensim.reflekt.internal.ReflektStore.superClasses
 import se.jensim.reflekt.internal.getClassFileLocators
 import se.jensim.reflekt.internal.packageFilter
+import java.lang.reflect.Constructor
+import java.lang.reflect.Field
+import java.lang.reflect.Member
+import java.lang.reflect.Method
 import java.util.concurrent.ConcurrentHashMap
+import java.util.function.Predicate
 
 class RefleKt(conf: RefleKtConf) {
 
@@ -15,12 +18,22 @@ class RefleKt(conf: RefleKtConf) {
     constructor(confDsl: RefleKtConf.() -> Unit) : this(RefleKtConf().apply(confDsl))
     constructor() : this(RefleKtConf())
 
-    private val allClasses = classFileLocators.flatMap { it.getClasses(conf.classFileLocatorConf.includeNestedJars) }
-    private val filteredClasses = allClasses.filter(packageFilter).toSet()
-    private val filteredClassRefs = getClasses(filteredClasses)
+    private val allClasses by lazy { classFileLocators.flatMap { it.getClasses(conf.classFileLocatorConf.includeNestedJars) } }
+    private val filteredClasses by lazy { allClasses.filter(packageFilter).toSet() }
+    private val filteredClassRefs by lazy { getClasses(filteredClasses) }
     private val subclassStore: MutableMap<Class<*>, Set<Class<*>>> = ConcurrentHashMap()
+    private val superclasses: MutableMap<String, Set<Class<*>>> = ConcurrentHashMap()
+    private val classAnnotations: MutableMap<String, Set<Class<*>>> = ConcurrentHashMap()
+    private val methodAnnotations: MutableMap<String, Set<Method>> = ConcurrentHashMap()
 
     fun getClassesAnnotatedWith(annotation: Class<out Annotation>): Set<Class<*>> = annotatedClasses(annotation)
+    /**
+     * Get all classes wearing an annotation
+     */
+    fun annotatedClasses(annotation: Class<out Annotation>): Set<Class<*>> = classAnnotations.computeIfAbsent(annotation.canonicalName) {
+        filteredClassRefs.filter { it.annotations.any { it.annotationClass.qualifiedName == annotation.canonicalName } }
+                .toSet()
+    }
 
     fun getSubClasses(clazz: Class<*>): Set<Class<*>> = subclassStore.computeIfAbsent(clazz) {
         filteredClassRefs.filter { isSubClassesOf(clazz, listOf(it)) }.toSet()
@@ -36,4 +49,30 @@ class RefleKt(conf: RefleKtConf) {
         }
         return isSubClassesOf(clazz, withSupers.flatten())
     }
+
+    fun superClasses(clazz: Class<*>): Set<Class<*>> =
+            superclasses.computeIfAbsent(clazz.canonicalName) {
+                (clazz.interfaces + clazz.superclass).filterNotNull().toSet()
+            }
+
+    fun getMethodsAnnotatedWith(annotation: Class<out Annotation>): Set<Method> = methodAnnotations.computeIfAbsent(annotation.canonicalName) {
+        filteredClassRefs
+                .flatMap { it.methods.toList() }
+                .filter { it.annotations.any { it::class.java == annotation } }
+                .toSet()
+    }
+
+    private fun getMethodsMatchParams(vararg paramClasses: Class<*>): Set<Method> = TODO()
+    private fun getMethodsReturn(clazz: Class<*>): Set<Method> = TODO()
+    private fun getMethodsWithAnyParamAnnotated(annotation: Class<out Annotation>): Set<Method> = TODO()
+    private fun getConstructorsAnnotatedWith(annotation: Class<out Annotation>): Set<Constructor<*>> = TODO()
+    private fun getConstructorsMatchParams(vararg paramClasses: Class<*>): Set<Constructor<*>> = TODO()
+    private fun getConstructorsWithAnyParamAnnotated(annotation: Class<out Annotation>): Set<Constructor<*>> = TODO()
+    private fun getFieldsAnnotatedWith(annotation: Class<out Annotation>): Set<Field> = TODO()
+    private fun getResources(predicate: Predicate<String>): Set<String> = TODO()
+    private fun getMethodParamNames(method: Method): List<String> = TODO()
+    private fun getConstructorParamNames(constructor: Constructor<*>): List<String> = TODO()
+    private fun getFieldUsage(field: Field): Set<Member> = TODO()
+    private fun getMethodUsage(method: Method): Set<Member> = TODO()
+    private fun getConstructorUsage(constructor: Constructor<*>): Set<Member> = TODO()
 }
--- src/main/kotlin/se/jensim/reflekt/internal/ReflektStore.kt
+++ src/main/kotlin/se/jensim/reflekt/internal/ReflektStore.kt
@@ -1,16 +1,17 @@
 package se.jensim.reflekt.internal
 
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicBoolean
 import java.util.stream.Collectors
 
 object ReflektStore {
 
+    private val classFuse = AtomicBoolean(false)
     private val classes: MutableMap<String, Class<*>?> = ConcurrentHashMap()
-    private val superclasses: MutableMap<String, Set<Class<*>>> = ConcurrentHashMap()
-    private val annotations: MutableMap<String, Set<Class<*>>> = ConcurrentHashMap()
 
-    fun getClass(clazz: String): Class<*>? = classes.computeIfAbsent(clazz) {
+    private fun getClass(clazz: String): Class<*>? = classes.computeIfAbsent(clazz) {
         try {
+            classFuse.compareAndSet(false, true)
             Class.forName(it)
         } catch (e: Exception) {
             null
@@ -25,22 +26,4 @@ object ReflektStore {
                 .filter { it != null }
                 .collect(Collectors.toSet()) as Set<Class<*>>
     else classes.mapNotNull { getClass(it) }.toSet()
-
-    fun superClasses(clazz: Class<*>): Set<Class<*>> =
-            superclasses.computeIfAbsent(clazz.canonicalName) {
-                (clazz.interfaces + clazz.superclass).filterNotNull().toSet()
-            }
-
-    fun annotatedClasses(annotation: Class<out Annotation>): Set<Class<*>> = annotations.computeIfAbsent(annotation.canonicalName) {
-        classes.values.filterNotNull()
-                .filter { it.annotations.any { it.annotationClass.qualifiedName == annotation.canonicalName } }
-                .toSet()
-    }
-
-    /*
-    fun noticeClasses(batch:Collection<Class<*>>){
-        batch.forEach { classes.putIfAbsent(it.canonicalName, it) }
-        //TODO("Add come async mechanism, coroutine?")
-    }
-    */
 }
