3a5c81683e8dda6341ad3fe63e877878878f99a3
Jens Brimfors <jens.brimfors@nordnet.se>
2019-06-18 06:35:29 +0200

    Added test to verify super deep directories/long package names

--- reflekt-core/src/test/java/se/jensim/reflekt/internal/ClassFileLocatorClassPathTest.java
+++ reflekt-core/src/test/java/se/jensim/reflekt/internal/ClassFileLocatorClassPathTest.java
@@ -44,6 +44,10 @@ public class ClassFileLocatorClassPathTest {
         assertTrue(result.contains(classfile));
     }
 
+    /**
+     * 416 levels of packages, if i counted correctly.
+     * @return class name (canonical) of the created class file
+     */
     private String createDeepDir() throws IOException {
         String alphabeth = "a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y.z.";
         for (int i = 0; i < 4; i++) {
