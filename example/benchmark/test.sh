436cd7a523ad0e0089e5206e927429704cf8ff1d
Jens Brimfors <jens.brimfors@nordnet.se>
2019-06-19 10:27:44 +0200

    Fix order of execution

--- example/benchmark/test.sh
+++ example/benchmark/test.sh
@@ -2,10 +2,11 @@
 
 echo "Building Example project with benchmarks"
 
+cd target
+
 JAR="$(ls  *jar-with-dependencies.jar | sort -V | tail -n1)"
 MAIN="com.example.Main"
 
-cd target
 #java -cp ${JAR} ${MAIN} 3 null        reflekt         org.reflections
 #java -cp ${JAR} ${MAIN} 3 null        org.reflections reflekt
 java -cp ${JAR} ${MAIN} 3 com.example RefleKt         org.reflections
