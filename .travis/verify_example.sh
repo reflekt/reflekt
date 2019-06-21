c17ef5fa54a5382db5ecce64416f30a72ce2e403
Jens Brimfors <jens.brimfors@nordnet.se>
2019-06-21 09:57:45 +0200

    Remove need for vcs-bumping the version, let travis do it when building tags

--- .travis/verify_example.sh
+++ .travis/verify_example.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-cd example/benchmark
+pushd example/benchmark
 ./test.sh
+popd
--- example/benchmark/test.sh
+++ example/benchmark/test.sh
@@ -2,7 +2,7 @@
 
 echo "Building Example project with benchmarks"
 
-cd target
+pushd target
 
 JAR="$(ls  *jar-with-dependencies.jar | sort -V | tail -n1)"
 MAIN="com.example.Main"
@@ -11,3 +11,5 @@ MAIN="com.example.Main"
 #java -cp ${JAR} ${MAIN} 3 null        org.reflections reflekt
 java -cp ${JAR} ${MAIN} 3 com.example RefleKt         org.reflections
 java -cp ${JAR} ${MAIN} 3 com.example org.reflections RefleKt
+
+popd
