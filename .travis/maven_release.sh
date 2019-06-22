6f65a2f4eab6a30035c3a02da1a5c82ef0743fdb
Jens Brimfors <jens.brimfors@nordnet.se>
2019-06-22 06:33:34 +0200

    Enable verbose bash script

--- .travis/maven_release.sh
+++ .travis/maven_release.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+
 set -e
+set -v
 
 openssl aes-256-cbc -K $encrypted_be6abfaeb7c9_key -iv $encrypted_be6abfaeb7c9_iv -in .travis/gpg/jens.brimfors-secret-gpg.key.enc -out jens.brimfors-secret-gpg.key -d
 gpg --import .travis/gpg/jens.brimfors-public-gpg.key
