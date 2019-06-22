8ae60c178710022d1975f8da981695ee2eebc49c
Jens Brimfors <jens.brimfors@nordnet.se>
2019-06-22 05:38:15 +0200

    Remove need for vcs-bumping the version, let travis do it when building tags

--- .travis/maven_install.sh
+++ .travis/maven_install.sh
@@ -3,12 +3,19 @@
 set -e
 
 if [[ -z "${TRAVIS_TAG}" ]]; then
-    echo "\${TRAVIS_TAG} not set, doing nothing.."
+    ./mvnw install
+    ./mvnw install -f example/pom.xml
 else
     ./mvnw -B release:update-versions -DdevelopmentVersion=${TRAVIS_TAG}-SNAPSHOT
+    ./mvnw install
     ./mvnw -B versions:update-parent -DallowSnapshots=true -DparentVersion=${TRAVIS_TAG}-SNAPSHOT -f example/pom.xml
     ./mvnw -B release:update-versions -DdevelopmentVersion=${TRAVIS_TAG}-SNAPSHOT -f example/pom.xml
     rm example/pom.xml.versionsBackup
     git add **/pom.xml
     git commit -m "Setting release version"
+    ./mvnw install -f example/pom.xml
 fi
+pushd example/benchmark
+. ./test.sh
+popd
+./mvnw sonar:sonar
--- .travis/verify_example.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-pushd example/benchmark
-. ./test.sh
-popd
