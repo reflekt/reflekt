219362b470ed2de6366f302475ed292db7660512
Jens Brimfors <jens.brimfors@nordnet.se>
2019-06-22 07:05:05 +0200

    Must not have any uncommitted files when performing a maven release

--- .travis/maven_install_release.sh
+++ .travis/maven_install_release.sh
@@ -9,6 +9,7 @@ mvn install
 mvn -B versions:update-parent -DallowSnapshots=true -DparentVersion=${TRAVIS_TAG}-SNAPSHOT -f example/pom.xml
 #mvn -B release:update-versions -DdevelopmentVersion=${TRAVIS_TAG}-SNAPSHOT -f example/pom.xml
 rm example/pom.xml.versionsBackup
+git add pom.xml
 git add **/pom.xml
 git commit -m "Setting release version"
 mvn install -f example/pom.xml
