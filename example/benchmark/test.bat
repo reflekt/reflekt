08d98b65ac7132587367b3f9abb3642b4f62c0fc
Jens <jens.brimfors@gmail.com>
2019-06-24 06:16:24 +0200

    Create a windows batch script for the benchmark

---
+++ example/benchmark/test.bat
@@ -0,0 +1,10 @@
+echo "Building Example project with benchmarks"
+@echo off
+
+SET JAR="example/benchmark/target/reflekt-benchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
+SET MAIN="com.example.Main"
+
+java -cp %JAR% %MAIN% 3 null        reflekt         org.reflections
+java -cp %JAR% %MAIN% 3 null        org.reflections reflekt
+java -cp %JAR% %MAIN% 3 com.example RefleKt         org.reflections
+java -cp %JAR% %MAIN% 3 com.example org.reflections RefleKt
