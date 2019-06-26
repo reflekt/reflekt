087b124be49d19f5baab8c15a0a73f1d212afe9c
Jens Brimfors <jens.brimfors@nordnet.se>
2019-06-26 06:39:54 +0200

    Update build scripts

--- .travis/maven_install_release.sh
+++ .travis/maven_install_release.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
+set -v
 
 echo "BUILDING RELEASE TAG $1"
 
--- example/benchmark/test.sh
+++ example/benchmark/test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+set -e
 echo "Building Example project with benchmarks"
 
 pushd target
